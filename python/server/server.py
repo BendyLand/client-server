6efca4b1713304f8b1db14b6689248e27cfcd694
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-13 19:56:34 -0400

    Move code into server file

--- python/server/server.py
+++ python/server/server.py
@@ -3,3 +3,18 @@ Echo Server and Client
 Project Description: An echo server simply sends back whatever data it receives from the client. This project will help you understand the basics of socket programming, how to set up a connection, and the basics of sending and receiving data over a network.
 Learning Outcomes: TCP/IP basics, socket programming, handling client-server connections.
 """
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return "index page!\n"
+
+@app.route("/hello")
+def hello():
+    return "hello page!\n"
+
+@app.route("/run")
+def run():
+    return "run page!\n"
